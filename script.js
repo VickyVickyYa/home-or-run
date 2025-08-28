a12f4ad7e07673e4df46719814125d71c11dfab5
Victoria <Victoria.Afe1@gmail.com>
2025-08-27 22:56:57 -0400

    fixed it now im making my other webpages right now tiger shark

--- script.js
+++ script.js
@@ -11,8 +11,11 @@ function checkAnimal() {
        "ribbon eel":"ribbon-eel.html",
        "weedy seadragon":"weedy-seadragon.html",
       "lionfish": "lionfish.html",
-       
-    };
+      "flower urchin":"flower-urchin.html",
+       "cone snail":"cone-snail.html",
+       "fang blennies":"fang-blennies.html",
+       "tiger shark":"tiger-shark.html",
+         };
 
     if (animals[animal]) {
         window.location.href = animals[animal];
