d62fd739dfa9bfc33617cf9859c17e73b4828444
Victoria <Victoria.Afe1@gmail.com>
2025-08-31 20:28:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,6 @@ function checkAnimal() {
     } else {
         document.getElementById("result").textContent = "I don’t know that animal yet!";
     }
-// Wait for the DOM to fully load
 document.addEventListener("DOMContentLoaded", function() {
     const audio = document.getElementById("runmusic");
     audio.play().catch((err) => {
